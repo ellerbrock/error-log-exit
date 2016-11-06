3d75ced29448ca8dc84dfcd69b461c0a6c33d88a
Maik Ellerbrock <opensource@frapsoft.com>
2016-11-06 04:26:14 +0100

    fix(babel): add babel2015 for older node version + travis fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@
 
 import 'colors'
 
-export default function errorLogExit (msg = 'something went wrong ...', exitCode = 1) {
-  console.log(`Error: ${msg}`.bold.red)
+export default function errorLogExit (msg, code) {
+  const errMsg = msg || 'something went wrong ...'
+  const exitCode = code || 1
+
+  console.log(`Error: ${errMsg}`.bold.red)
   process.exit(exitCode)
 }
